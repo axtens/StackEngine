2d291797e5a4fdc6b9385a4b6a8d816c8017f951
Bruce M. Axtens <bugmagnet@outlook.com>
2020-06-17 20:44:23 +0800

    Add project files.

---
+++ Opcodes.cs
@@ -0,0 +1,228 @@
+﻿using SymbolTable;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackEngine
+{
+    public partial class Engine
+    {
+        public void DUPE()
+        {
+            DataNeeds(1);
+            DATA.Push(DATA.Peek());
+            return;
+        }
+        public void DROP()
+        {
+            DataNeeds(1);
+            DATA.Pop();
+            return;
+        }
+        public void SWAP()
+        {
+            DataNeeds(2);
+            var swap1 = DATA.Pop();
+            var swap2 = DATA.Pop();
+            DATA.Push(swap1);
+            DATA.Push(swap2);
+            return;
+        }
+        public void OVER()
+        {
+            DataNeeds(2);
+            DATA.Push(DATA.ElementAt(1));
+            return;
+        }
+        public void ROTE()
+        {
+            DataNeeds(3);
+            var rot1 = DATA.Pop();
+            var rot2 = DATA.Pop();
+            var rot3 = DATA.Pop();
+            DATA.Push(rot2);
+            DATA.Push(rot1);
+            DATA.Push(rot3);
+            return;
+        }
+        public void NOT_ROTE()
+        {
+            DataNeeds(3);
+            var rot1 = DATA.Pop();
+            var rot2 = DATA.Pop();
+            var rot3 = DATA.Pop();
+            DATA.Push(rot1);
+            DATA.Push(rot3);
+            DATA.Push(rot2);
+            return;
+        }
+        public void NIP()
+        {
+            SWAP();
+            DROP();
+            return;
+        }
+        public void TUCK()
+        {
+            SWAP();
+            OVER();
+            return;
+        }
+
+        private void TWO_ROT()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TWO_SWAP()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TWO_TUCK()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TWO_OVER()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TWO_DUPE()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TWO_NIP()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TWO_DROP()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ROLL()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void QUESTION_DUPE()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PICK()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ARITHOP()
+        {
+            Needing(new SymbolType[] { SymbolType.STRING, SymbolType.DOUBLE, SymbolType.DOUBLE });
+            var op = DATA.Pop();
+            var lh = DATA.Pop();
+            var rh = DATA.Pop();
+            switch (op.Value.ToString())
+            {
+                case "+":
+                    DATA.Push(SYMTAB.CreateSymbol(SymbolType.DOUBLE, (double)lh.Value + (double)rh.Value));
+                    break;
+                case "-":
+                    DATA.Push(SYMTAB.CreateSymbol(SymbolType.DOUBLE, (double)lh.Value - (double)rh.Value));
+                    break;
+                case "*":
+                    DATA.Push(SYMTAB.CreateSymbol(SymbolType.DOUBLE, (double)lh.Value * (double)rh.Value));
+                    break;
+                case "/":
+                    DATA.Push(SYMTAB.CreateSymbol(SymbolType.DOUBLE, (double)lh.Value / (double)rh.Value));
+                    break;
+            }
+        }
+
+        private void ATDICT()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ASSTRING()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ASNUM()
+        {
+            DataNeeds(1);
+            var val = DATA.Pop();
+            DATA.Push(SYMTAB.CreateSymbol(SymbolType.DOUBLE, double.Parse(val.Value.ToString())));
+        }
+
+        private void END()
+        {
+            DataNeeds(1);
+            var chr = DATA.Pop();
+        }
+
+        private void CHR()
+        {
+            DataNeeds(1);
+            Check(0, SymbolType.DOUBLE);
+            var chr = DATA.Pop();
+            DATA.Push(SYMTAB.CreateSymbol(SymbolType.STRING, (char)((int)chr.Value)));
+        }
+
+        private void Check(int v, SymbolType t)
+        {
+            var elt = DATA.ElementAt(v);
+            if (elt.Type != t)
+            {
+                throw new Exception($"Type error element {v}: needs {t}");
+            }
+        }
+
+        private void DOT()
+        {
+            DataNeeds(1);
+            Console.Write($"{DATA.Pop().Value}");
+        }
+
+        private void BANGSIGN()
+        {
+            DataNeeds(2);
+            Check(0, SymbolType.STRING);
+            SYMTAB.UpdateEntry(DATA.Pop().Value.ToString(), DATA.Pop());
+        }
+
+        private void ATSIGN()
+        {
+            DataNeeds(1);
+            Check(0, SymbolType.STRING);
+            var varname = DATA.Pop().Value.ToString();
+            if (SYMTAB.ExistsEntry(varname))
+            {
+                var ent = SYMTAB.RetrieveEntry(varname);
+                DATA.Push(ent);
+            }
+            else
+            {
+                throw new Exception($"No entry in symbol table called {varname}");
+            }
+
+        }
+
+        public void DEF()
+        {
+            var defname = DATA.Pop().Value
+                               .ToString();
+            var defvalue = DATA.Pop().Value;
+            SYMTAB.UpdateEntry(defname, SymbolType.STRING, defvalue);
+        }
+
+
+    }
+}
---
+++ StackEngine.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using SymbolTable;
+
+namespace StackEngine
+{
+    public partial class Engine
+    {
+        public static SymTab SYMTAB;
+        public static Stack<Symbol> DATA = new Stack<Symbol>();
+        public static Stack<Symbol> RETURN = new Stack<Symbol>();
+        public static Dictionary<string, object> opcodeDict;
+
+        public Engine()
+        {
+            opcodeDict = new Dictionary<string, object>()
+            {
+                {"DROP",(Action)DROP},
+                {"NIP", (Action)NIP},
+                {"DUP", (Action)DUPE},
+                {"OVER",(Action)OVER},
+                {"TUCK",(Action)TUCK},
+                {"SWAP",(Action)SWAP},
+                {"PICK",(Action)PICK}, //not implemented
+
+                {"ROT", (Action)ROTE},
+                {"-ROT",(Action)NOT_ROTE},
+                {"?DUP",(Action)QUESTION_DUPE },
+                {"ROLL",(Action)ROLL},
+                {"2DROP",(Action)TWO_DROP},
+                {"2NIP",(Action)TWO_NIP},
+                {"2DUP",(Action)TWO_DUPE},
+                {"2OVER",(Action)TWO_OVER},
+                {"2TUCK",(Action)TWO_TUCK},
+                {"2SWAP",(Action)TWO_SWAP},
+                {"2ROT",(Action)TWO_ROT},
+
+                {"!", (Action)BANGSIGN},
+                {"@", (Action)ATSIGN},
+                {".", (Action)DOT},
+                {"DEF", (Action)DEF},
+                {"CHR", (Action)CHR},
+                {"END", (Action)END },
+
+
+                {"ASNUM", (Action)ASNUM },
+                {"ASTRING",(Action)ASSTRING},
+                {"@DICT",(Action)ATDICT },
+                {"ARITHOP", (Action)ARITHOP }
+
+            };
+        }
+
+        public Engine Parse(string script)
+        {
+            var tokens = Regex.Matches(script, @"[\""].+?[\""]|['].+?[']|\S+", RegexOptions.Singleline).Cast<Match>().Select(m => m.Value).ToList();
+            foreach (var token in tokens)
+            {
+
+                //if (int.TryParse(token, out int intVal))
+                //{
+                //    DATA.Push(SYMTAB.CreateSymbol(SymbolTable.SymbolType.INTEGER, intVal));
+                //    continue;
+                //}
+                //
+                //if (double.TryParse(token, out double dblVal))
+                //{
+                //    DATA.Push(SYMTAB.CreateSymbol(SymbolTable.SymbolType.DOUBLE, dblVal));
+                //    continue;
+                //}
+                //
+                //if (bool.TryParse(token, out bool boolVal))
+                //{
+                //    DATA.Push(SYMTAB.CreateSymbol(SymbolTable.SymbolType.BOOLEAN, boolVal));
+                //    continue;
+                //}
+
+                if (opcodeDict.ContainsKey(token))
+                {
+                    ((Action)opcodeDict[token])();
+                }
+                else if (SYMTAB.ExistsEntry(token))
+                {
+                    SymtabEvaluate(token);
+                }
+                else
+                {
+                    var tempToken = token;
+                    if (tempToken.Substring(0, 1) == "\"" && tempToken.Substring(tempToken.Length - 1) == "\"")
+                    {
+                        tempToken = token.Substring(1, tempToken.Length - 2);
+                    }
+                    DATA.Push(SYMTAB.CreateSymbol(SymbolTable.SymbolType.STRING, tempToken));
+
+                }
+
+            }
+            return this;
+        }
+
+        public void SetSymTab(ref SymTab st)
+        {
+            SYMTAB = st;
+        }
+
+        public Engine SymtabEvaluate(string token)
+        {
+            if (!SYMTAB.ExistsEntry(token))
+            {
+                throw new Exception($"{token}??");
+            }
+            else
+            {
+                if (SYMTAB.RetrieveEntry(token).Type == SymbolType.ACTION)
+                    ((Action)SYMTAB.RetrieveEntry(token).Value)();
+                else
+                { // if it's a value, just recover it. But how do you tell?
+                    Parse(SYMTAB.RetrieveEntry(token).Value.ToString());
+                }
+            }
+            return this;
+        }
+
+        public void Needing(SymbolType[] types)
+        {
+            if (types.Length < DATA.Count)
+            {
+                throw new Exception($"Operation requires {types.Length} parameters but only {DATA.Count} provided!");
+            }
+            for (var e = 0; e < DATA.Count; e++)
+            {
+                if (DATA.ElementAt(e).Type != types[e])
+                {
+                    throw new Exception($"Stack element {e} requires {types[e]}!");
+                }
+            }
+        }
+
+        public Symbol Pop()
+        {
+            return DATA.Pop();
+        }
+
+        public void Push(Symbol s)
+        {
+            DATA.Push(s);
+        }
+
+        public int Count()
+        {
+            return DATA.Count;
+        }
+
+        public Boolean Needs(int n)
+        {
+            return this.Count() >= n;
+        }
+
+        public Engine AddOpcode(string name, Action action)
+        {
+            opcodeDict.Add(name, action);
+            return this;
+        }
+    }
+}
